a8c1d3f125c22deba1981c6b33329d2d8ce06bdb
kumankoishi.py <7iggif@naver.com>
2020-03-26 22:41:46 +0900

    Practicing Variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
+var n: String = "Top Level Variable"
+val nullableString: String? = null      // Nullable String
+
 fun main() {
-    println("Hello Kotlin");
+    println("Hello Kotlin")
+
+    var id: String = "513"       // var: variable that can reassign
+    val name: String = "kuman"   // val: read only variable
+
+    id = "514"
+    println(name)
+    println(id)
+    println(name+id)
+
+    //var n: String = "Local Variable"
+    n = "Hello kuman514"
+    println(n)
 }

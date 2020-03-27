9aaf541a640722ad57fed2faee24345638024083
kumankoishi.py <7iggif@naver.com>
2020-03-27 09:41:35 +0900

    Practicing Collections and Iterations

--- src/Main.kt
+++ src/Main.kt
@@ -27,6 +27,20 @@ fun sayHello3(greeting: String, item: String) = println("$greeting $item.")
 fun sayHello() : Unit {
     println(getGreeting())
 }
+
+fun sayHello4(greeting: String, itemsToGet: List<String>) {
+    itemsToGet.forEach { itemToGet ->
+        println("$greeting, $itemToGet!")
+    }
+}
+
+fun sayHello5(greeting: String, vararg itemsToGet: String) {
+    // Check out "vararg"
+    // treated as an array
+    itemsToGet.forEach { itemToGet ->
+        println("$greeting, $itemToGet!")
+    }
+}
 // ======================================
 
 fun main() {
@@ -80,4 +94,50 @@ fun main() {
     println(getAnotherGreeting2("greeters."))
     sayHello2("ParameterName")
     sayHello3("our baby","koishi")
+
+    // Collections and Iterations ===========
+    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
+    println(interestingThings.size)
+    println(interestingThings[0])
+    println(interestingThings.get(0))
+
+    for(interestingThing in interestingThings) {
+        println(interestingThing)
+    }
+
+    interestingThings.forEach {
+        println(it)
+    }
+
+    interestingThings.forEach { interestingThing ->
+        println(interestingThing)
+        // lambda syntax within Kotlin
+    }
+
+    interestingThings.forEachIndexed { index, interestingThing ->
+        println("$interestingThing is at index $index")
+    }
+
+    val amusingThings = listOf("Kotlin", "Programming", "Comic Books")      // unmutable (can't add)
+    amusingThings.forEach { amusingThing ->
+        println("$amusingThing is Fun!")
+    }
+
+    val anotherAmusement = mutableListOf("Dogs", "Yasuo", "Koishi")         // mutable (can add)
+    anotherAmusement.add("kuman")
+
+    val map = mapOf(1 to "a", 2 to "b", 3 to "c")                           // unmutable (can't put)
+    map.forEach { (key, value) ->
+        println("$key to $value")
+    }
+
+    val anotherMap = mutableMapOf(1 to "a", 2 to "b", 3 to "c")             // mutable (can put)
+    anotherMap.put(4, "d")
+
+    sayHello4("Hello", amusingThings)
+    sayHello4("Hello", anotherAmusement)
+    sayHello5("Hello")                                              // Doesn't have to pass varargs
+    sayHello5("Hey", "koishi", "kotlin", "kuman")       // But also can pass multiple arguments
+    sayHello5("Hi", *interestingThings)                             // Array to varargs
+    // ======================================
 }

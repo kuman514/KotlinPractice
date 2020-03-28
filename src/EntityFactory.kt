8db80e68dff469a2183cd48bcca6bb9aa8501ceb
kuman514 <7iggif@naver.com>
2020-03-28 16:08:49 +0900

    Practicing Enum Class

--- src/EntityFactory.kt
+++ src/EntityFactory.kt
@@ -1,6 +1,23 @@
+import java.util.*
+
+// enum class
+enum class EntityType {
+    EASY, MEDIUM, HARD
+}
+
 // object declaration
 object EntityFactory {
     //fun create() = Entity("514", "kuman")
+    fun create(type: EntityType): Entity {
+        val id = UUID.randomUUID().toString()
+        val name = when(type) {
+            EntityType.EASY -> "Easy"
+            EntityType.MEDIUM -> "Medium"
+            EntityType.HARD -> "Hard"
+        }
+
+        return Entity(id, name)
+    }
 }
 
 class Entity(val id: String, val name: String) {
@@ -10,8 +27,14 @@ class Entity(val id: String, val name: String) {
 }
 
 fun main() {
-    val entity = EntityFactory.create()
-    println(entity)
+    val easyEntity = EntityFactory.create(EntityType.EASY)
+    println(easyEntity)
+
+    val mediumEntity = EntityFactory.create(EntityType.MEDIUM)
+    println(mediumEntity)
+
+    val hardEntity = EntityFactory.create(EntityType.HARD)
+    println(hardEntity)
 }
 
 /*

c41f94f273c24c18329778addc6d041cefc2bdfc
kuman514 <7iggif@naver.com>
2020-03-29 22:40:48 +0900

    Practicing Higher Order Functions

--- src/HighLevelFunctions.kt
+++ src/HighLevelFunctions.kt
@@ -1,3 +1,4 @@
+/*
 fun printFilteredStrings(list: List<String>, predicate: (String) -> Boolean) {
     list.forEach {
         if(predicate(it)) {
@@ -5,12 +6,36 @@ fun printFilteredStrings(list: List<String>, predicate: (String) -> Boolean) {
         }
     }
 }
+*/
+
+fun printFilteredStrings(list: List<String>, predicate: ((String) -> Boolean)?) {
+    println("printFilteredStrings")
+    list.forEach {
+        if(predicate?.invoke(it) == true) {
+            println(it)
+        }
+    }
+}
+
+val predicate: (String) -> Boolean = {
+    it.endsWith("n")
+}
+
+// this higher order function can work as both inputs and outputs
+fun getPrintPredicate(): (String) -> Boolean {
+    return { it.startsWith("j") }
+}
 
 fun main() {
-    val list = listOf("kotlin", "java", "c++", "javascript")
+    val list = listOf("kotlin", "java", "c++", "javascript", "python")
 
     //printFilteredStrings(list, { it.startsWith("j") })
     printFilteredStrings(list) {
         it.startsWith("j")
     }
+
+    // higher order functions are useful for optional inputs
+    printFilteredStrings(list, null)
+    printFilteredStrings(list, predicate)
+    printFilteredStrings(list, getPrintPredicate())
 }
